2f0dd25581a00747a467c14ebb5bc8d246aa5ebc
Dani <dgarciaestebanez@gmail.com>
2026-05-28 17:14:07 +0200

    Vuelta al commit anterior

---
+++ tareasDTO/src/main/java/empresallm/tareasdto/TareasDtoApplication.java
@@ -0,0 +1,13 @@
+package empresallm.tareasdto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareasDtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TareasDtoApplication.class, args);
+    }
+
+}
---
+++ tareasDTO/src/test/java/empresallm/tareasdto/TareasDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package empresallm.tareasdto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareasDtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
